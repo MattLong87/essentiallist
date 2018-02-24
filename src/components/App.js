ef6eb22444bc54d5b32629685a1e745899f7ef59
Matthew Long <MattLong87@gmail.com>
2018-02-24 10:33:51 -0500

    refactored app to flow from quizzes stored in app component state

--- src/components/App.js
+++ src/components/App.js
@@ -7,19 +7,34 @@ import '../css/reset.css'
 import '../css/styles.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        quizzes: [
+          {
+            title: "Top 100 Board Games",
+            verb: "played",
+            selections: ["Puerto Rico", "Agricola", "Arkham Horror", "Pandemic", "Gloomhaven"]
+          },
+          {
+            title: "Top 100 Movies",
+            verb: "seen",
+            selections: ["Citizen Kane", "No Country For Old Men", "Groundhog Day"]
+          }
+        ]
+    }
+  }
+
   render() {
 
-    var quiz = {
-      title: "Top 100 Board Games",
-      verb: "played",
-      selections: ["Puerto Rico", "Agricola", "Arkham Horror", "Pandemic", "Gloomhaven"]
-    };
+    var routes = this.state.quizzes.map((quiz, key) => <Route exact path={"/" + quiz.title.replace(/\s+/g, '-')} render={()=><QuizPage quiz={quiz}/>}/>);
+    var titles = this.state.quizzes.map((quiz, key) => quiz.title);
 
     return (
       <Router>
         <div>
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path={"/" + quiz.title.replace(/\s+/g, '-')} render={()=><QuizPage quiz={quiz}/>}/>
+          <Route exact path="/" render={()=><LandingPage titles={titles}/>} />
+          {routes}
         </div>
       </Router>
     );
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,30 +2,21 @@ import React from 'react';
 import QuizButton from './QuizButton';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-export default class LandingPage extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            quizzes: ["Top 100 Board Games", "Top 100 Movies", "Top 100 Books", "Top 50 Albums"]
-        }
-    }
+export default function LandingPage(props) {
     
-    render(){
-        let quizButtons = this.state.quizzes.map((title, key) => {
-            var titleConcat = title.replace(/\s+/g, '-');
-            return <Link to={titleConcat}><QuizButton quizName={title} key={key}/></Link>;
-        });
+    let quizButtons = props.titles.map((title, key) => {
+        var titleConcat = title.replace(/\s+/g, '-');
+        return <Link to={titleConcat}><QuizButton quizName={title} key={key}/></Link>;
+    });
 
-        return (
-            <div className="landing-page">
-                <h1>EssentialList</h1>
-                <h2>Only The Best</h2>
-                <hr />
-                <div className="quizzes-area">
-                    {quizButtons}
-                </div>
+    return (
+        <div className="landing-page">
+            <h1>EssentialList</h1>
+            <h2>Only The Best</h2>
+            <hr />
+            <div className="quizzes-area">
+                {quizButtons}
             </div>
-        )
-    }
-
+        </div>
+    )
 }
